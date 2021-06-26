9ff74f9c8905a8907196052aa80837c69d6aea47
Daniel_Vas <daniel23_cruza@hotmail.com>
2021-06-26 00:04:43 -0500

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var log = Singleton.Log.Instance;
+			log.Save("a");
+			log.Save("b");
+		}
+	}
+}
